6c5f8f2b27bd41cbe0f33fa6827b15825df010af
Dan Connolly <dconnolly@kumc.edu>
2013-03-25 23:25:23 -0500

    preserve comments in python to scala transformation

--- p2s.py
+++ p2s.py
@@ -1,8 +1,10 @@
 '''p2s -- convert python lexical syntax to scala
 '''
 
+import StringIO
 import ast
 import logging
+import tokenize
 
 log = logging.getLogger(__name__)
 
@@ -16,39 +18,76 @@ def main(argv, open, stdout,
 
 def convert(infn, src, out):
     t = ast.parse(src, infn)
-    p2s = PyToScala(out)
+    tl = PyToScala.tokens_per_line(src)
+    p2s = PyToScala(out, tl)
     p2s.visit(t)
 
 
 class PyToScala(ast.NodeVisitor):
-    def __init__(self, out):
+    def __init__(self, out, token_lines):
         self._out = out
+        self._lines = list(token_lines)
+        self._line_ix = 0
+        self._node_row = 0
+
+    @classmethod
+    def tokens_per_line(cls, src):
+        row = 1
+        line = []
+        readline = StringIO.StringIO(src).readline
+        for tok in tokenize.generate_tokens(readline):
+            toknum, tokval, (tokrow, col), rc, l = tok
+            if tokrow > row:
+                yield (row, line)
+                line = []
+                row = tokrow
+            line.append(tok)
+        if line:
+            yield (row, line)
+
+    def _sync(self, node):
+        wr = self._out.write
+        if isinstance(node, ast.expr) or isinstance(node, ast.stmt):
+            while node.lineno >= self._lines[self._line_ix][0]:
+                for tok in self._lines[self._line_ix][1]:
+                    if tok[0] == tokenize.COMMENT:
+                        wr('//' + tok[1] + '\n')
+                self._line_ix += 1
+        return wr
+
+    def newline(self):
+        wr = self._out.write
+        wr('\n')
 
     def visit_Module(self, node):
-        wr = self._out.write
+        wr = self._sync(node)
         wr('object __name__ {\n')
         for stmt in node.body:
             self.visit(stmt)
-        wr('}\n')
+        wr('}')
+        self.newline()
 
     def visit_FunctionDef(self, node):
         limitation(not node.decorator_list)
-        wr = self._out.write
-        wr('\n\ndef %s(' % node.name)
+        wr = self._sync(node)
+        self.newline()
+        self.newline()
+        wr('def %s(' % node.name)
         self.visit(node.args)
-        wr('): Any = {\n')
+        wr('): Any = {')
+        self.newline()
         for stmt in node.body:
             self.visit(stmt)
         wr('}')
 
     def visit_Return(self, node):
         # Return(expr? value)
-        wr = self._out.write
+        wr = self._sync(node)
         wr('return')
         if node.value:
             wr(' ')
             self.visit(node.value)
-        wr('\n')
+        self.newline()
 
     def visit_Delete(self, node):
         # Delete(expr* targets)
@@ -56,7 +95,7 @@ class PyToScala(ast.NodeVisitor):
             self.visit(expr)
 
     def visit_Assign(self, node):
-        wr = self._out.write
+        wr = self._sync(node)
         if len(node.targets) > 1:
             wr('(')
             for e in node.targets:
@@ -67,12 +106,12 @@ class PyToScala(ast.NodeVisitor):
 
         wr(' = ')
         self.visit(node.value)
-        wr('\n')
+        self.newline()
 
     def visit_Print(self, node):
         # Print(expr? dest, expr* values, bool nl)
         limitation(not node.dest)
-        wr = self._out.write
+        wr = self._sync(node)
         wr('println' if node.nl else 'print')
         wr('(')
         sep = ''
@@ -80,113 +119,130 @@ class PyToScala(ast.NodeVisitor):
             wr(sep)
             self.visit(expr)
             sep = ' + '
-        wr(')\n')
+        wr(')')
+        self.newline()
 
     def visit_For(self, node):
         # For(expr target, expr iter, stmt* body, stmt* orelse)
-        wr = self._out.write
+        wr = self._sync(node)
         wr('for (')
         self.visit(node.target)
         wr(' <- ')
         self.visit(node.iter)
-        wr(') {\n')
+        wr(') {')
+        self.newline()
         for stmt in node.body:
             self.visit(stmt)
-        wr('}\n')
+        wr('}')
+        self.newline()
+
 
         if node.orelse:
             wr('/* for ... else: */\n')
+            self.newline()
             wr('if ((')
             self.visit(node.iter)
-            wr(').isEmpty) {\n')
+            wr(').isEmpty) {')
+            self.newline()
             for stmt in node.orelse:
                 self.visit(stmt)
-            wr('}\n')
+            wr('}')
+            self.newline()
 
     def visit_While(self, node):
         # While(expr test, stmt* body, stmt* orelse)
         limitation(not node.orelse)
-        wr = self._out.write
+        wr = self._sync(node)
         wr('while (')
         self.visit(node.test)
-        wr(') {\n')
+        wr(') {')
+        self.newline()
         for stmt in node.body:
             self.visit(stmt)
-        wr('}\n')
+        wr('}')
+        self.newline()
 
     def visit_If(self, node):
         # If(expr test, stmt* body, stmt* orelse)
-        wr = self._out.write
+        wr = self._sync(node)
         wr('if (')
         self.visit(node.test)
-        wr(') {\n')
+        wr(') {')
+        self.newline()
         for stmt in node.body:
             self.visit(stmt)
         wr('}')
         if node.orelse:
-            wr(' else {\n')
+            wr(' else {')
+            self.newline()
             for stmt in node.orelse:
                 self.visit(stmt)
             wr('}')
-        wr('\n')
+        self.newline()
 
     def visit_Raise(self, node):
         # Raise(expr? type, expr? inst, expr? tback)
         limitation(not node.tback and node.type)
 
-        wr = self._out.write
+        wr = self._sync(node)
         wr('throw new')
         self.visit(node.type)
         wr('(')
         if node.inst:
             self.visit(node.inst)
-        wr(')\n')
+        wr(')')
+        self.newline()
 
     def visit_Assert(self, node):
         # Assert(expr test, expr? msg)
-        wr = self._out.write
+        wr = self._sync(node)
         wr('assert(')
         self.visit(node.test)
         if node.msg:
             wr(', ')
             self.visit(node.msg)
-        wr(')\n')
+        wr(')')
+        self.newline()
 
     def visit_Import(self, node):
-        wr = self._out.write
+        wr = self._sync(node)
         for name in node.names:
             wr('import ')
             self.visit(name)
-            wr('\n')
+            self.newline()
 
     def visit_Global(self, node):
         # Global(identifier* names)
-        wr = self._out.write
+        wr = self._sync(node)
         wr('/* global ')
         wr(', '.join(node.names))
-        wr(' */\n')
+        wr(' */')
+        self.newline()
 
     def visit_Expr(self, node):
-        wr = self._out.write
+        self._sync(node)
         self.visit(node.value)
-        wr('\n')
+        self.newline()
 
     def visit_Pass(self, node):
-        wr = self._out.write
-        wr('/* pass */\n')
+        wr = self._sync(node)
+        wr('/* pass */')
+        self.newline()
 
     def visit_Break(self, node):
-        wr = self._out.write
-        wr('break\n')
+        wr = self._sync(node)
+        wr('break')
+        self.newline()
 
     def visit_Continue(self, node):
-        wr = self._out.write
-        wr('continue\n')
+        wr = self._sync(node)
+        wr('continue')
+        self.newline()
 
     def visit_BoolOp(self, node):
         # BoolOp(boolop op, expr* values)
         # boolop = And | Or
-        wr = self._out.write
+        wr = self._sync(node)
         sym = {ast.And: '&&', ast.Or: '||'}[node.op.__class__]
         sep = ''
         for expr in node.values:
@@ -198,7 +254,7 @@ class PyToScala(ast.NodeVisitor):
         # BinOp(expr left, operator op, expr right)
         # operator = Add | Sub | Mult | Div | Mod | Pow | LShift 
         #         | RShift | BitOr | BitXor | BitAnd | FloorDiv
-        wr = self._out.write
+        wr = self._sync(node)
         wr('(')  # parens?
         self.visit(node.left)
         wr(' ')
@@ -216,12 +272,12 @@ class PyToScala(ast.NodeVisitor):
         # unaryop = Invert | Not | UAdd | USub
         limitation(isinstance(node.op, ast.Not))
 
-        wr = self._out.write
+        wr = self._sync(node)
         wr('! ')
         self.visit(node.operand)
 
     def visit_Dict(self, node):
-        wr = self._out.write
+        wr = self._sync(node)
         wr('Map(')
         for k, v in zip(node.keys, node.values):
             self.visit(k)
@@ -232,7 +288,7 @@ class PyToScala(ast.NodeVisitor):
     def visit_Compare(self, node):
         # Compare(expr left, cmpop* ops, expr* comparators)
         # compop = Eq | NotEq | Lt | LtE | Gt | GtE | Is | IsNot | In | NotIn
-        wr = self._out.write
+        wr = self._sync(node)
         sep = ''
         left = node.left
         for op, expr in zip(node.ops, node.comparators):
@@ -264,7 +320,7 @@ class PyToScala(ast.NodeVisitor):
 	#		 expr? starargs, expr? kwargs)
 
         # TODO: change x.update(y) to x ++= y
-        wr = self._out.write
+        wr = self._sync(node)
         self.visit(node.func)
         wr('(')
         sep = ''
@@ -278,11 +334,11 @@ class PyToScala(ast.NodeVisitor):
                    not node.kwargs)
 
     def visit_Num(self, node):
-        wr = self._out.write
+        wr = self._sync(node)
         wr(str(node.n))
 
     def visit_Str(self, node):
-        wr = self._out.write
+        wr = self._sync(node)
         s = node.s
         limitation('"""' not in s)
 
@@ -295,14 +351,14 @@ class PyToScala(ast.NodeVisitor):
 
     def visit_Attribute(self, node):
         # Attribute(expr value, identifier attr, expr_context ctx)
-        wr = self._out.write
+        wr = self._sync(node)
         self.visit(node.value)
         wr('.')
         wr(node.attr)
 
     def visit_Subscript(self, node):
         # Subscript(expr value, slice slice, expr_context ctx)
-        wr = self._out.write
+        wr = self._sync(node)
         self.visit(node.value)
         slice = node.slice
         limitation(slice.__class__ in (ast.Index, ast.Slice))
@@ -330,9 +386,10 @@ class PyToScala(ast.NodeVisitor):
 
     def visit_Name(self, node):
         # hmm... ctx
-        wr = self._out.write
+        wr = self._sync(node)
         if isinstance(node.ctx, ast.Store):
             wr('val ')
+        # TODO: fix True, False to true, false
         wr(node.id)
 
     def visit_List(self, node):
@@ -340,7 +397,7 @@ class PyToScala(ast.NodeVisitor):
 
     def visit_Tuple(self, node, o=None):
         # Tuple(expr* elts, expr_context ctx)
-        wr = self._out.write
+        wr = self._sync(node)
         if o:
             wr(o)
         wr('(')
@@ -352,7 +409,7 @@ class PyToScala(ast.NodeVisitor):
         wr(')')
 
     def visit_arguments(self, node):
-        wr = self._out.write
+        wr = self._sync(node)
         limitation(not node.vararg and
                    not node.kwarg)
 
@@ -368,7 +425,7 @@ class PyToScala(ast.NodeVisitor):
             sep = ', '
 
     def visit_alias(self, node):
-        wr = self._out.write
+        wr = self._sync(node)
         wr(node.name)
         if node.asname:
             wr(' as ')
