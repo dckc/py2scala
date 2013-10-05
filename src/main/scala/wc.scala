4afcdc1a1d04d0304db16d8b6107ef43b294269a
Dan Connolly <http://www.madmode.com/>
2013-10-05 02:01:40 -0500

    regenerate wc.scala

--- src/main/scala/wc.scala
+++ src/main/scala/wc.scala
@@ -1,70 +1,74 @@
-import __fileinfo__._
-import com.madmode.py2scala.__builtin__._
+import scala.collection.mutable
 import com.madmode.py2scala.{batteries => py}
+import com.madmode.py2scala.__builtin__._
+import wc_fileinfo._
 
 object wc {
   import py.StringIO
   
-  /** 
-    >>> caps = Mock.caps()
-    >>> main(**caps)
-    >>> caps['stdout'].getvalue()
-    '3 f1\n'
-
-    :type argv: IndexedSeq[String]
-    :type open_arg: String => File
-    :type stdout: File
-    */
-  def main(argv: IndexedSeq[String], open_arg: String => File, stdout: File) = {
+  /**
+  >>> world = Mock()
+  >>> _, stdout = world.with_caps(main)
+  >>> stdout.getvalue()
+  '3 f1\n'
+  
+  :type argv: IndexedSeq[String]
+  :type open_arg: String => Iterable[String]
+  :type stdout: File
+  */
+  def main(argv: IndexedSeq[String], stdout: File, open_arg: String => Iterable[String]) = {
     for (filename <- argv.drop(1)) {
       val stream = open_arg(filename)
       val qty = sum(for (line <- stream) yield 1)
-      stdout.println(qty + filename)
+      print(stdout, qty, filename)
       }
     }
   
-  class Mock {
+  case class Mock() {
+    self =>
     
-    /*@@ @classmethod */
-    def caps(/*cls: Any*/) = {
-      val argv = List("prog", "f1")
+    /**
+    :type f: (IndexedSeq[String], File, String => Iterable[String]) => Unit
+    */
+    def with_caps(f: (IndexedSeq[String], File, String => Iterable[String]) => Unit) = {
+      val argv = mutable.IndexedSeq("prog", "f1")
       
-      /** :type x: String*/
+      /**
+      :type x: String
+      */
       def open_arg(x: String) = {
         if (! argv.contains(x)) {
-          throw new IOError("FIXME")
+          throw new IOError()
           }
-        List("line1", "line2", "line3")
+        mutable.IndexedSeq("line1", "line2", "line3")
         }
-      val out = StringIO.StringIO("KLUDGE")
-      dict(argv=argv.clone(), stdout=out, open_arg=open_arg)
+      val out = StringIO.StringIO()
+      (f(argv.drop(0), out, open_arg _), out)
       }
     }
   if (__name__ == "__main__") {
     
-    def _with_caps() = {
+    /**
+    :type main: (IndexedSeq[String], File, String => Iterable[String]) => Unit
+    */
+    def _with_caps(main: (IndexedSeq[String], File, String => Iterable[String]) => Unit) = {
       import py.sys.{argv, stdout}
       
-      /** :type arg: String*/
+      /**
+      :type arg: String
+      */
       def open_arg(arg: String) = {
         if (! argv.contains(arg)) {
           throw new IOError("only paths given as arguments can be opened")
           }
         open(arg)
         }
-      
-      /** 
-            :type main: (IndexedSeq[String], File, String => File) => Unit
-            */
-      def with_caps(main: (IndexedSeq[String], File, String => File) => Unit) = {
-        main(argv=argv.clone(), stdout=stdout, open_arg=open_arg)
-        }
-      with_caps _
+      main(argv.drop(0), stdout, open_arg)
       }
-    _with_caps(main)
+    _with_caps(main _)
     }
   }
 
-object __fileinfo__ {
+object wc_fileinfo {
   val __name__ = "wc"
-}
+  }
