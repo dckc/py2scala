e55a1f119be49d61c964d331476230afee91f981
Dan Connolly <dconnolly@kumc.edu>
2013-10-05 18:08:10 -0500

    # Note: [] isn't well-typed unless we constrain the parameter type # TODO: use has_type([], 'Iterable[Int]')

--- test/for_else.py
+++ test/for_else.py
@@ -1,4 +1,7 @@
-for x in []:
+
+# Note: [] isn't well-typed unless we constrain the parameter type
+# TODO: use has_type([], 'Iterable[Int]')
+for x in [1]:
     print "some"
 else:
     print "none"
